2f6cafef86c4413f7ea745efac1c711fa79af8f7
AngieRai <168058225+AngieRai@users.noreply.github.com>
2024-06-06 11:32:28 +0545

    vvv

---
+++ components/card.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+<v-card>
+    <v-card-title>{{}}</v-card-title>
+    <v-img :src="e" cover>
+    </v-img>
+</v-card>
+    </div>
+</template>
+
+<script setup>
+defineprops([]);
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <section>
+      <v-row>
+        <v-col>
+          <v-card>
+            <v-card-title>{{}}</v-card-title>
+            <v-img :src="e" cover> </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </section>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+const { data: product } = usefetch(
+    ""
+):
+<style scoped>
+</style>
