3c51a88b025dc11877444ace1343e219abd50772
AngieRai <168058225+AngieRai@users.noreply.github.com>
2024-06-17 11:30:09 +0545

    hjhj

--- pages/products/store.vue
+++ pages/products/store.vue
@@ -2,26 +2,45 @@
   <div>
     <section>
       <v-container>
-        <h1>Add Product</h1>
+        <h1 class="py-5">Add Product</h1>
+        <!-- {{productData}} -->
         <v-row>
           <v-col cols="12" md="6">
-            <v-text-field label="Enter Title"></v-text-field>
+            <v-text-field label="Enter Title" v-model="productData.title"></v-text-field>
           </v-col>
           <v-col cols="12" md="6">
-            <v-text-field label="Enter Price"></v-text-field>
+            <v-text-field label="Enter Price" v-model="productData.Price"></v-text-field>
           </v-col>
           <v-col cols="12" md="6">
-            <v-text-field label="Enter Title"></v-text-field>
+            <v-text-field label="Enter Description" v-model="productData.Description"></v-text-field>
           </v-col>
           <v-col cols="12" md="6">
-            <v-text-field label="Enter Title"></v-text-field>
+            <v-text-field label="Enter image" v-model="productData.image"></v-text-field>
+          </v-col>
+           <v-col cols="12" md="6">
+            <v-text-field label="Enter category" v-model="productData.category"></v-text-field>
           </v-col>
         </v-row>
+
+        <v-btn @click="productStore.postProduct(productData)">
+          Add Product
+        </v-btn>
       </v-container>
     </section>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const productData = ref({
+    title: null,
+    price: null,
+    description: null,
+    image: null,
+    category : null,
+});
+
+import { useProductStore } from "~/stores/products";
+const productStore = useProductStore();
+</script>
 
 <style scoped></style>
--- stores/products.js
+++ stores/products.js
@@ -4,7 +4,7 @@ import { axiosAPi } from "~/config/axios";
 export const useProductStore = defineStore("product", {
   state: () => ({
     products: [],
-    AllProducts:[]
+    AllProducts: [],
   }),
 
   actions: {
@@ -17,6 +17,21 @@ export const useProductStore = defineStore("product", {
       try {
         let response = await axiosAPi.get("products");
         this.AllProducts = response.data;
+      } catch (e) {
+        console.warn(e);
+      }
+    },
+
+    async postProduct(productData) {
+      try {
+        let response = await axiosAPi.post("products", productData);
+        if (response.status == 200) {
+          alert("Product Added Successfully!");
+          console.warn(response.data);
+          navigateTo("/products");
+        } else {
+          alert("Something went Wrong!");
+        }
       } catch (e) {
         console.warn(e);
       }
